9727104e0c52bd1546bbcf06360865e134a7341b
Ayush Bhatt <ayushbhatt11001100@gmail.com>
2024-08-01 14:46:20 +0530

    done

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,10 +1,10 @@
 import { toast } from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
-import {add ,remove} from "../redux/Slices/CartSlice";
+import { add, remove } from "../redux/Slices/CartSlice";
 
-const Product = ({post}) => {
+const Product = ({ post }) => {
 
-  const {cart} = useSelector((state) => state);
+  const { cart } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   const addToCart = () => {
@@ -24,7 +24,7 @@ const Product = ({post}) => {
         <p className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">{post.title}</p>
       </div>
       <div>
-        <p className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0,10).join(" ") + "..."}</p>
+        <p className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0, 10).join(" ") + "..."}</p>
       </div>
       <div className="h-[180px]">
         <img src={post.image} className="h-full w-full " />
@@ -34,28 +34,28 @@ const Product = ({post}) => {
         <div>
           <p className="text-yellow-600 font-semibold">${post.price}</p>
         </div>
-        
+
         {
           cart.some((p) => p.id == post.id) ?
-          (<button
-          className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
+            (<button
+              className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
           text-[12px] p-1 px-3 uppercase 
           hover:bg-gray-700
           hover:text-white transition duration-300 ease-in"
-          onClick={removeFromCart}>
-            Remove Item
-          </button>) :
-          (<button
-          className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
+              onClick={removeFromCart}>
+              Remove Item
+            </button>) :
+            (<button
+              className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
           text-[12px] p-1 px-3 uppercase 
           hover:bg-gray-700
           hover:text-white transition duration-300 ease-in"
-          onClick={addToCart}>
-            Add to Cart
-          </button>)
+              onClick={addToCart}>
+              Add to Cart
+            </button>)
         }
       </div>
-     
+
 
     </div>
   );
